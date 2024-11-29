e9a12631909096463d5b97a3e4fdedc0bd1859b9
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2024-11-29 12:56:54 +0600

    add create project route and create project schema

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,22 +1,60 @@
 'use client'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { api } from '@/trpc/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
-type formInput = {
+type FormInput = {
     repoUrl: string,
     projectName: string,
     githubToken?: string
 }
 
 export const CreatePage = () => {
-    const { register, handleSubmit, reset } = useForm<formInput>()
+    const { register, handleSubmit, reset } = useForm<FormInput>()
+    const createProject = api.project.createProject.useMutation()
+
+    function onSubmit(data: FormInput){
+      window.alert(JSON.stringify(data,null,2));
+      createProject.mutate({
+        name: data.projectName,
+        githubUrl: data.repoUrl,
+        githubToken: data.githubToken
+      }, {
+        onSuccess: () => {
+          toast.success('Project created successfully')
+          reset()
+        },
+        onError: () => {
+          toast.error('Error creating project')
+        }
+      })
+      return true;
+    }
+
   return (
-    <div className='flex items-center gap-12 h-full justify-center'>
+    <div className='flex min-h-screen items-center justify-center p-4'>
+      <div className='flex flex-col-reverse md:flex-row items-center gap-8 md:gap-12 max-w-3xl w-full'>
         <img 
           src='/backgrounds/create-bg-4.png' 
-          className='fixed left-1/2 md:left-[55%] top-1/2 -translate-x-1/2 -translate-y-1/2 w-40 h-auto opacity-70'
+          className='w-auto h-40 md:h-56 opacity-70'
           alt="Background graphic"
         />
+        <div className='w-full max-w-md space-y-6'>
+          <div className='space-y-2'>
+            <h1 className='font-semibold text-2xl'>Link your Github Repository</h1>
+            <p className='text-sm text-muted-foreground'>Enter the link of your repository to link it to GitSage.</p>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)} className='space-y-3'>
+            <Input {...register('projectName',{required: true})} placeholder='Project Name' required />
+            <Input {...register('repoUrl',{required: true})} placeholder='Github URL' required type='url' />
+            <Input {...register('githubToken')} placeholder='Github Token (Optional)' />
+            <Button type='submit' className='w-full'>Create Project</Button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import { createTRPCRouter, protectedProcedure } from './../trpc';
+import { PrismaClient } from '@prisma/client';
+import {create} from "domain"
+export const projectRouter = createTRPCRouter({
+    createProject: protectedProcedure.input(
+        z.object({
+            name: z.string(),
+            githubUrl: z.string(),
+            githubToken: z.string().optional(),
+        })
+    ).mutation(async({ctx, input}) => {
+        const project = await ctx.db.project.create({
+            data: {
+                name: input.name,
+                githubUrl: input.githubUrl,
+                userToProjects: {
+                    create: {
+                        userId: ctx.user.userId!,
+                    }
+                }
+            }
+        })
+        return project
+})
+});
